9a6327c97fb299f6b66ae0bc5e8e4e7e79b9d97c
Eric.Zhao <Eric.Zhao@activenetwork.com>
2019-04-30 11:58:39 +0800

    Multi clipboard

---
+++ study/fileio/MultiClipboard/mcb.py
@@ -0,0 +1,31 @@
+#! python3
+'''
+mcb.pyw  save and load pieces of text to clipboard
+usage:
+py.exe mcb.pyw save <keyword>  save clipboard to keyword
+py.exe mcb.pyw <keyword>  load keyword to clipboard
+py.exe mcb.pyw list   load all keywords to clipboard
+
+'''
+
+import shelve, pyperclip, sys
+
+def mcb() :
+    mcbFile = shelve.open('mcb')
+
+    # save clipboard content
+    if len(sys.argv) == 3 and sys.argv[1].lower() == 'save' :
+        mcbFile[sys.argv[2]] = pyperclip.paste()
+    elif len(sys.argv) == 2 :
+        if sys.argv[1].lower() == 'list' :
+            pyperclip.copy(str(list(mcbFile.keys() )))
+        elif sys.argv[1] in mcbFile :
+            pyperclip.copy(mcbFile[sys.argv[1]] )
+            
+    mcbFile.close()
+
+
+def testMcb() :
+    mcb()
+
+testMcb()
